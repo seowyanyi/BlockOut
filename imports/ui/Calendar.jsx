c1cf0eea638bfbf290e6f75e4cebbcb8463e6014
Louis Puah <lauises@gmail.com>
2016-06-25 23:51:01 +0800

    Event Details Complete

--- imports/ui/Calendar.jsx
+++ imports/ui/Calendar.jsx
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
 
 export default class Calendar extends Component {
+	  goToChat() {
+	    browserHistory.push('/chat')
+	  }
+	  goToSettings() {
+	    browserHistory.push('/settings')
+	  }  
+	  goBack() {
+	    browserHistory.push('/home/chatmap')
+	  }
+
   render() {
     return (
+        <div className="apphome calendar">
         <div>
-          Calendar here
+          <div className="topbar">
+            <h2>Calendar</h2>
+          </div>
         </div>
+        
+        <div className="calendar-content">
+        	<img src="cal.jpg" />
+        </div>
+
+        <div className="bottombar">
+          <button onClick={this.goBack.bind(this)}><i className="fa fa-home"></i></button>
+          <button onClick={this.goToChat.bind(this)}><i className="fa fa-comment-o"></i></button>
+          <button className="currentPage"><i className="fa fa-calendar"></i></button>
+          <button><i onClick={this.goToSettings.bind(this)} className="fa fa-bars"></i></button>
+        </div>
+      </div>
       )
   }
 }
--- imports/ui/EventDetail.jsx
+++ imports/ui/EventDetail.jsx
@@ -3,11 +3,66 @@ import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 
 export default class EventDetail extends Component {
+	goToEventList() {
+	    browserHistory.push('/eventlist')
+	  }
+
   render() {
     return (
-        <div>
-        Event Detail here
-        </div>
+        <div className="apphome eventDetail">
+	        <div>
+	          <div className="topbar">
+	          <div onClick={this.goToEventList.bind(this)}  className="backButton">
+	            <i className="fa fa-angle-left clickable"></i>
+	          </div>	
+	            <h2>Event Detail</h2>
+	            <i className="fa fa-search clickable"></i>
+	          </div>
+	        </div>
+	        <div className="feature">
+	        	<img src="eventBreakfast.jpg" />
+	        	<h2>Mdm Tan's Breakfast</h2>
+	        </div>
+	        <div className="dateTime">
+		        <div className="date">
+		        	<i className="fa fa-calendar clickable"></i>
+		        	<span>28 JUN 2016</span>
+		        </div>
+		        <div className="time">
+		        	<i className="fa fa-clock-o clickable"></i>
+		        	<span>08:30AM</span>
+		        </div>
+	        </div>
+	        <div className="hr"></div>
+	        <div className="priceTime">
+		        <div className="price">
+		        	<h3>Price</h3>
+		        	<p>$5</p>
+		        </div>
+	        </div>
+	        <div className="hr"></div>
+	        <div className="info">
+		        <h4>Description</h4>
+		        <p>Local breakfast with homemade toast & savoury spreads. Pick up these treats at the void deck before you leave for your busy day at work.</p>
+		        <div className="hosted">
+			        <span>Hosted By</span>
+			        <div className="host"><img src="./eventHost.png" /><span>Ann Tan</span></div>
+		        </div>
+	        </div>
+	        <div className="hr"></div>
+	        <div className="guests">
+		        <h4>Guest List</h4>
+		        <div className="guestList">
+			        <img src="./user1.jpg" />
+			        <img src="./user3.jpg" />
+			        <img src="./user5.jpg" />
+			        <i className="fa fa-angle-right clickable"></i>
+		        </div>
+	        </div>
+	        <div className="info">
+	        	<button className="join">Join!</button>
+	        </div>
+	      </div>
       )
   }
 }
