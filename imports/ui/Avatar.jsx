b58fff058e540550c47b4f9ae2c911f047a18871
Jin Zhe <spyrant@gmail.com>
2016-06-25 18:07:07 +0800

    style for avatars

--- imports/ui/Avatar.jsx
+++ imports/ui/Avatar.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Logo from './Logo.jsx';
 
 export default class Avatar extends Component {
   render() {
     let content = null
     if (this.props.displayName) {
       content = this.props.displayName[0]
-    } 
-    return (
-        <div className="author-icon">
+    }
+    if (this.props.displayName === 'BlockOut') {
+      return (
+        <div className="author-icon blockout">
+          <Logo />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className="author-icon" style={{backgroundColor: localStorage.avatarBgColor}}>
           {content}
         </div>
       )
+    }
   }
 }
