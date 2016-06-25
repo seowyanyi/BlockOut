785c1fc5c4d17ee33b561bfeabe9af25461e1198
seowyanyi <seow.yanyi@gmail.com>
2016-06-25 17:35:56 +0800

    fix index error

--- imports/ui/MessageSection.jsx
+++ imports/ui/MessageSection.jsx
@@ -25,44 +25,48 @@ class MessageSection extends Component {
     let messageListItems = []
     if (this.props.messages && this.props.messages.length > 0) {
       let filteredMessages = this.props.messages.filter(msg => msg.postalCode === localStorage.postalCode && msg.subGroupName === localStorage.subGroupName)
-      // first message
-      let message = filteredMessages[0]
-      let nextName = filteredMessages.length > 1 ? filteredMessages[1].authorName : null
-      messageListItems.push(
-        <MessageListItem
-          key={message._id}
-          message={message}
-          showIcon={message.authorName == nextName ? false : true}
-          showName={true}
-        />
-      )
-      for (let i=1; i<filteredMessages.length-1; ++i) {
-        let message = filteredMessages[i]
-        let nextName = filteredMessages[i+1].authorName
-        let prevName = filteredMessages[i-1].authorName
+      
+      if (filteredMessages.length > 0) {
+        // first message
+        let message = filteredMessages[0]
+        let nextName = filteredMessages.length > 1 ? filteredMessages[1].authorName : null
         messageListItems.push(
           <MessageListItem
             key={message._id}
             message={message}
             showIcon={message.authorName == nextName ? false : true}
-            showName={message.authorName != prevName ? true : false}
+            showName={true}
           />
         )
-      }
+        for (let i=1; i<filteredMessages.length-1; ++i) {
+          let message = filteredMessages[i]
+          let nextName = filteredMessages[i+1].authorName
+          let prevName = filteredMessages[i-1].authorName
+          messageListItems.push(
+            <MessageListItem
+              key={message._id}
+              message={message}
+              showIcon={message.authorName == nextName ? false : true}
+              showName={message.authorName != prevName ? true : false}
+            />
+          )
+        }
 
-      // last message
-      if (filteredMessages.length > 1) {
-        message = filteredMessages[filteredMessages.length-1]
-        prevName = filteredMessages.length > 1 ? filteredMessages[filteredMessages.length-2].authorName : null
-        messageListItems.push(
-          <MessageListItem
-            key={message._id}
-            message={message}
-            showIcon={true}
-            showName={message.authorName != prevName ? true : false}
-          />
-        )
+        // last message
+        if (filteredMessages.length > 1) {
+          message = filteredMessages[filteredMessages.length-1]
+          prevName = filteredMessages.length > 1 ? filteredMessages[filteredMessages.length-2].authorName : null
+          messageListItems.push(
+            <MessageListItem
+              key={message._id}
+              message={message}
+              showIcon={true}
+              showName={message.authorName != prevName ? true : false}
+            />
+          )
+        }
       }
+  
     }
 
     return (
