16db72b0b73861f0cb9d5e7cd8f781075b73cbeb
Louis Puah <lauises@gmail.com>
2016-06-25 11:05:58 +0800

    Added Favicon
    -added BlockOut name

--- imports/ui/LoginSection.jsx
+++ imports/ui/LoginSection.jsx
@@ -29,6 +29,7 @@ export default class LoginSection extends Component {
     return (
       <div className="login-section">
       <Logo />
+      <h2>BlockOut</h2>
       <h3>Hi there!</h3>
         <form>
           <fieldset>
